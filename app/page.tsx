f66e90410fd00b8461ad62da91bcfa0c44cf7071
lucky99ethan <ethansawe99@gmail.com>
2024-04-08 22:00:15 +0300

    one

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,194 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from 'react';
+import { Amplify } from "aws-amplify";
+import { generateClient as apiClientGenerator, GraphQLResult, GraphQLSubscription } from "aws-amplify/api";
+import { AiOutlineDelete, AiTwotoneEdit } from "react-icons/ai";
+import { CSVLink } from 'react-csv';
+import { Button, View, withAuthenticator } from "@aws-amplify/ui-react";
+import { createStock as createStockMutation, deleteStock as deleteStockMutation, updateStock as updateStockMutation } from "../../../src/graphql/mutations";
+import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
+import config from "../src/amplifyconfiguration.json";
+import "@aws-amplify/ui-react/styles.css";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+Amplify.configure(config);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const initialData = {
+  id: "",
+  name: "",
+  price: "",
+  category: "",
+};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Item {
+  id: string;
+  name: string;
+  price: string;
+  category: string;
 }
+
+const initialCategories: string[] = ["Electronics", "Clothing", "Books"];
+
+const CrudComponent: React.FC = () => {
+  const [data, setData] = useState<Item[]>([]);
+  const [editingItem, setEditingItem] = useState<string | null>(null);
+
+
+  const [categories, setCategories] = useState<string[]>(initialCategories);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>('');
+  const [inputPrice, setInputPrice] = useState<string>('');
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPrice(e.target.value);
+  };
+
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '' && inputPrice.trim() !== '') {
+      const newItem: Item = { id: Date.now().toString(), name: inputValue, price: inputPrice, category: selectedCategory };
+      setData([...data, newItem]);
+      setInputValue('');
+      setInputPrice('');
+      setSelectedCategory('');
+    }
+  };
+
+  const handleEditItem = (id: string) => {
+    const itemToEdit = data.find(item => item.id === id);
+    if (itemToEdit) {
+      setInputValue(itemToEdit.name);
+      setInputPrice(itemToEdit.price);
+      setSelectedCategory(itemToEdit.category);
+    
+      setEditingItem(id);
+    }
+  };
+
+  const handleUpdateItem = () => {
+    if (editingItem !== null && inputValue.trim() !== '' && inputPrice.trim() !== '') {
+      setData(data.map(item => {
+        if (item.id === editingItem) {
+          return { ...item, name: inputValue, price: inputPrice, category: selectedCategory };
+        }
+        return item;
+      }));
+      setInputValue('');
+      setInputPrice('');
+      setSelectedCategory('');
+      setEditingItem(null);
+    }
+  };
+
+  const handleDeleteItem = (id: string) => {
+    setData(data.filter(item => item.id !== id));
+  };
+
+  // Filter data based on search query
+  const filteredData = data.filter(item =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  // CSV data for download
+  const csvData: any[] = [
+    ['Product Name', 'Price', 'Category'],
+    ...filteredData.map(item => [item.name, item.price, item.category])
+  ];
+
+  return (
+    <div className="mt-10">
+      <div className="flex mb-4 justify-center items-center">
+      <label htmlFor="categorySelect">Choose a category:</label>
+      <select 
+        id="categorySelect"
+        value={selectedCategory} 
+        onChange={handleCategoryChange} 
+        className="p-2 border select select-primary rounded mr-2 w-40"
+        aria-label="Category selection"
+      >
+        <option value="">Select Category</option>
+        {categories.map((category, index) => (
+          <option key={index} value={category}>{category}</option>
+        ))}
+      </select>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter item name"
+          className="p-2 border border-gray-300 rounded mr-2 color-primary"
+        />
+        <input
+          type="text"
+          value={inputPrice}
+          onChange={handlePriceChange}
+          placeholder="Enter the price"
+          className="p-2 border border-gray-300 rounded mr-2"
+        />
+        {editingItem !== null ? (
+          <button onClick={handleUpdateItem} className="btn btn-primary mr-4 mb-2 ">
+            Update
+          </button>
+        ) : (
+          <button onClick={handleAddItem} className="btn btn-primary mr-4 mb-2">
+            Add
+          </button>
+        )}
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search items"
+          className="p-2 border border-gray-300 rounded mr-2"
+        />
+        <CSVLink data={csvData} filename={"product_data.csv"} className="btn btn-primary mr-2 mb-2" target="_blank">
+          Download CSV
+        </CSVLink>
+      </div>
+
+      <div>
+        <table className="table-fixed border-collapse border border-gray-400 w-full">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="px-4 py-2 w-1/3">Product Name</th>
+              <th className="px-4 py-2 w-1/4">Price</th>
+              <th className="px-4 py-2 w-1/4">Category</th>
+              <th className="px-4 py-2 w-1/4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map(item => (
+              <tr key={item.id} className="border-b border-gray-400">
+                <td className="px-4 py-2">{item.name}</td>
+                <td className="px-4 py-2">${item.price}</td>
+                <td className="px-4 py-2">{item.category}</td>
+                <td className="px-4 py-2">
+                <button onClick={() => handleEditItem(item.id)} className="btn btn-primary mr-2" aria-label="Edit item">
+                <AiTwotoneEdit />
+              </button>
+              <button onClick={() => handleDeleteItem(item.id)} className="btn btn-primary" aria-label="Delete item">
+                <AiOutlineDelete />
+              </button>
+              
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default CrudComponent;
